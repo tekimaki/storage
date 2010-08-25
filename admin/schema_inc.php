1c74478fddb5a51067355b418a679e61228fd3fb
tekimaki <will@tekimaki.com>
2010-08-25 10:26:29 -0400

    clean up annoying kernel spoof of installer class

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,10 +1,9 @@
 <?php
 
-global $gBitInstaller;
+global $gBitSystem;
 
-$gBitInstaller->registerPackageInfo( STORAGE_PKG_NAME, array(
+$gBitSystem->registerPackageInfo( STORAGE_PKG_NAME, array(
 	'description' => 'Directory to store information to the file system.',
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
 ) );
 
-?>
